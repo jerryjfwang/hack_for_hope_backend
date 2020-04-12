1a8237728d88acf5eee9a9d9da3c132796a85a24
Jerry Wang <jerry@jerrys-mbp.lan>
2020-04-11 19:50:28 -0700

    Adding ability to get all grocery stores in LA neighborhoods

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const mongoose = require("mongoose"); // ORM to interface MongoDB
 const morgan = require("morgan"); // Package for logging incoming client API requests
 const bodyParser = require("body-parser"); // Package to allow Express to parse request body
 
+const getNeighborhoods = require("./utils/getNeighborhoods");
+
 /* Create server instance */
 const server = express();
 
@@ -29,6 +31,35 @@ server.use(bodyParser.json()); // Allows Express to parse request body
 
 /* Set up appropriate routers */
 server.get("/", (req, res) => res.send("Hello world!"));
+server.get("/recommendation", async (req, res) => {
+  const latitude = 34.04403; // user location
+  const longitude = -118.25672; // user location
+  const maxDuration = 25; // in minutes
+  const daysLeft = 6; // days left until groceries run out
+  const travelMethod = "driving"; // driving, walking, bicylcing
+
+  // Get all LA neighborhoods
+  const neighborhoods = await getNeighborhoods();
+
+  // Get all grocery stores in each neighborhood
+  const allGroceryStores = require("./neighborhoods.json");
+  neighborhoods.forEach(
+    (neighborhood) =>
+      (neighborhood.groceryStores = allGroceryStores[neighborhood.name])
+  );
+
+  // Get all minimum durations to those grocery stores
+
+  // Filter out grocery stores that will take longer than user's maxDuration to get to
+
+  // Sort neighborhoods by asc # of cases
+
+  // Sort all the grocery stores in the top three neighborhoods by ratio of rating per # of ratings
+
+  // Return the top three three grocery stores with the optimal busy times
+
+  res.json({ neighborhoods });
+});
 
 /* Initalize server instance to listen for requests on specified port */
 server.listen(process.env.SERVER_PORT || 8080, () =>
